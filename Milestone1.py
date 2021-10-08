27925b1e822fc1da659f792449aa9cca18919227
Sanchez3002 <92134766+Sanchez3002@users.noreply.github.com>
2021-10-08 18:32:08 -0500

    Update Milestone1.py

--- Milestone1.py
+++ Milestone1.py
@@ -181,8 +181,8 @@ def source_rna(protein):
 
 #12
 def splice_rna( dna, intron_list ):
-    for intron in intron_list:
-        dna = dna.replace( intron, '' )
-    protein = rna2codon( rna )
+    for i in intron_list:
+        dna = dna.replace( i, '' )
+    pro = rna2codon( rna )
     rna = dna2rna( dna )
-    return protein
+    return pro
